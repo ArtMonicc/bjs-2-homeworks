68fff0568e617fe45a618e60e5248a0188941a20
Mesrop <105871863+ArtMonicc@users.noreply.github.com>
2023-03-17 22:25:53 +0300

    ДЗ Декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,41 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+  function wrapper(...args) {
+    const hash = md5(args);
+    const objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+    let result = func(...args);
+    cache.push({ hash: hash, value: result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
+function cachingDecoratorNew(func) {
+  let cache = [];
+  function wrapper(...args) {
+    const hash = md5(args);
+    const objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+    let result = func(...args);
+    cache.push({ hash: hash, value: result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
