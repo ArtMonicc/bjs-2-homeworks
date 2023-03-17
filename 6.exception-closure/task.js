5645cf8faf0a84cdef4bbf05f253b1b2603d7a91
Mesrop <105871863+ArtMonicc@users.noreply.github.com>
2023-03-17 21:48:17 +0300

    ДЗ Обработка

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿// Задача #1
+
+function parseCount(value) {
+  if (isNaN(Number.parseFloat(value))) {
+    throw new Error("невалидное значение");
+  }
+  return Number.parseFloat(value);
+}
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+// Задача #2
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b < c || b + c < a || a + c < b) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  get perimetr() {
+    return this.a + this.b + this.c;
+  }
+  get area() {
+    const perimetr = this.a + this.b + this.c;
+    const p = perimetr / 2;
+    return Number(
+      Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3)
+    );
+  }
+}
+function getTriangle(a, b, c) {
+  try {
+    new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get area() {
+        return "ошибка!!! Треугольник не существует";
+      },
+      get perimeter() {
+        return "ошибка!!! Треугольник не существует";
+      },
+    };
+  }
+  return new Triangle(a, b, c);
+}
